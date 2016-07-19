7d5a4bbfe4185c17534c5e6350440d689a57bc0c
NathanRomike <nathanromike@gmail.com>
2016-07-19 15:50:48 -0700

    Remove new socket instance

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var socket = new io.Socket();
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
