f066091276090a00d124605b4e4cb64d2ba5503e
NathanRomike <nathanromike@gmail.com>
2016-07-19 15:37:45 -0700

    change http listen to assigned port.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+http.listen(process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -14,8 +16,4 @@ io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
-});
-
-http.listen(3000, function() {
-  console.log('listening on *:3000');
 });
