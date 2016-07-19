01186ec67ab752be261ce9be7b24385ccdb05d67
NathanRomike <nathanromike@gmail.com>
2016-07-19 15:47:26 -0700

    Add initial new io.Socket.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var socket = new io.Socket();
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
